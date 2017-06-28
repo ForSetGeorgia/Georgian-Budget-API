25b639119cec04a63f7a885990d5e324a7909f7d
Antarya <antarya@gmail.com>
2017-06-28 12:37:07 +0400

    Staging deploy branch updated

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ set :user, 'budget-staging'
 set :application, 'Budget-API-Staging'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/JumpStartGeorgia/Georgian-Budget'
-set :branch, 'master'
+set :branch, 'inmedia'
 set :web_url, ENV['STAGING_WEB_URL']
 set :visible_to_robots, false
 set :use_ssl, true
