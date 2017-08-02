7ca6b8ff50138c438c80dc255d047d998c682402
Antarya <antarya@gmail.com>
2017-08-02 17:38:18 +0400

    Production branch updated

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ set :user, 'budget'
 set :application, 'Budget-API-Production'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/ForSetGeorgia/Georgian-Budget-API'
-set :branch, 'master'
+set :branch, 'inmedia'
 set :web_url, ENV['PRODUCTION_WEB_URL']
 set :use_ssl, true
 set :puma_worker_count, '2'
