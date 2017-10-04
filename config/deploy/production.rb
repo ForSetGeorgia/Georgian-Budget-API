b05bca8acb993e1c66525cec81e7522a5ab6d480
Antarya <antarya@gmail.com>
2017-10-04 11:54:13 +0400

    First deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
-set :domain, 'delta.jumpstart.ge'
+set :domain, 'alpha.forset.ge'
 set :user, 'budget'
-set :application, 'Budget-API-Production'
+set :application, 'Budget-API'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/ForSetGeorgia/Georgian-Budget-API'
-set :branch, 'inmedia'
+set :branch, 'master'
 set :web_url, ENV['PRODUCTION_WEB_URL']
 set :use_ssl, true
 set :puma_worker_count, '2'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,9 @@
-set :domain, 'alpha.jumpstart.ge'
-set :user, 'budget-staging'
-set :application, 'Budget-API-Staging'
+set :domain, 'beta.forset.ge'
+set :user, 'budget'
+set :application, 'Budget-API'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/ForSetGeorgia/Georgian-Budget-API'
-set :branch, 'inmedia'
+set :branch, 'master'
 set :web_url, ENV['STAGING_WEB_URL']
 set :visible_to_robots, false
 set :use_ssl, true
