b71c5f218c050794466dfcfb8742a6dc21b48a3e
Antarya <antarya@gmail.com>
2018-02-08 10:34:43 +0400

    Prepare app to use https
    Flag use_ssl and config.force_ssl set to true

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
-set :domain, '159.65.22.44'
+set :domain, 'biujeti-api.forset.ge'
 set :user, 'deploy'
 set :application, 'Budget-API'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/ForSetGeorgia/Georgian-Budget-API'
 set :branch, 'master'
 set :web_url, ENV['PRODUCTION_WEB_URL']
-set :use_ssl, false
+set :use_ssl, true
 set :puma_worker_count, '1'
 set :puma_thread_count_min, '1'
 set :puma_thread_count_max, '4'
