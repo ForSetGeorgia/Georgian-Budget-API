8a0f85e4d97295fd420f72e134a2d9c670b06771
Antarya <antarya@gmail.com>
2018-02-12 14:06:51 +0400

    Prepare production for first deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
-set :domain, 'biujeti-api.forset.ge'
+set :domain, '178.62.74.79'
 set :user, 'deploy'
 set :application, 'Budget-API'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/ForSetGeorgia/Georgian-Budget-API'
 set :branch, 'master'
 set :web_url, ENV['PRODUCTION_WEB_URL']
-set :use_ssl, true
+set :use_ssl, false
 set :puma_worker_count, '1'
 set :puma_thread_count_min, '1'
 set :puma_thread_count_max, '4'
